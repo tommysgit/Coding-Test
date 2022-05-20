4f54cccc87802fd8cde9061bcd5f96bdbf1fed22
Youngil <84563221+tommysgit@users.noreply.github.com>
2022-05-20 09:50:54 +0900

    [Silver II] Title: 최대 힙, Time: 200 ms, Memory: 45980 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,16 @@
+import heapq
+import sys
+N = int(sys.stdin.readline())
+heap = []
+prints = []
+for i in range(N):
+    X = int(sys.stdin.readline())
+    if X !=0 :
+        heapq.heappush(heap,(-X, X))
+    else:
+        if len(heap) == 0:
+            prints.append((0,0))
+        else:
+            prints.append(heapq.heappop(heap))
+for i in range(len(prints)):
+    print(prints[i][1])
