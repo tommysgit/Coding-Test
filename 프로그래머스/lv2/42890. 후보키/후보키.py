329f5dc828484d48faf2e372cdf32c0b9aff278d
YoungIlGwak <84563221+tommysgit@users.noreply.github.com>
2022-08-03 20:30:22 +0900

    [level 2] Title: 후보키, Time: 1.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42890. 후보키/후보키.py
+++ 프로그래머스/lv2/42890. 후보키/후보키.py
@@ -10,17 +10,17 @@ def solution(relation):
         for i in range(len(list1)):
             tmp_list.append(list1[i] + list2[i])
         return copy.deepcopy(tmp_list)
+    # 컬럼의 개수만큼 반복문
     for i in range(len(relation[0])):
         # 유일성 : i+1개의 속성이 고유해야함
         # 최소성 : 유일성이 최소값이어야함 
-        # i번째를 지날때 마다 
         primary = []
+        # 후보키가 가능한 경우의 수 조합을 만든다.
         for j in list(itertools.combinations(target, i+1)):
             j = list(j)
             is_continue = 0
-            # fix의 원소가 j에 있으면 continue
+            # fix안의 값은 최소성을 만족하는 후보키 이므로 j조합은 후보키가 될 수 없다.
             for e in fix:
-                #print(set(j) , set(e) , len(j) , len(e))
                 if len(set(j) - set(e)) == len(j) - len(e):
                     is_continue = 1
                     break
